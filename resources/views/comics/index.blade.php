fbe100dffd9dbf143a05f5fd1ef2820e1cd04ad7
Angelo <Angelodimauro91@gmail.com>
2022-11-13 21:56:10 +0100

    show.blade.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comics</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista Comics</title>
 </head>
+
 <body>
-    @dump($comics)
+    @foreach ($comics as $comic)
+        <div>
+            <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
+        </div>
+    @endforeach
 </body>
+
 </html>
