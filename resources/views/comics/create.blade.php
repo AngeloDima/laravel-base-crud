39b080ee3a7e3b24f70248921be68c3eb7ae62be
Angelo <Angelodimauro91@gmail.com>
2022-11-15 02:18:18 +0100

    validatore crud 4 (finish)

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,39 +5,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Crea Comic</title>
 </head>
 
 <body>
+    @if ($errors->any())
+        <div>
+            Controlla il form, Ci sono degli errori
+        </div>
+    @endif
+
+
     <form method="POST" action="{{ route('comics.store') }}">
         @csrf
-        <div>
+        <div @error('title') class="is-invalid"  @enderror>
             <label for="title">Title</label>
-            <input type="text"max="150" required name="title">
+            <input type="text" name="title" maxlength="150" required value="{{ old('title', '') }}">
+            @error('title')
+                <div>
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('description') class="is-invalid"  @enderror>
             <label for="description">Description</label>
-            <textarea name="description" required cols="30" rows="10"></textarea>
+            <textarea name="description" required cols="30" rows="10">{{ old('description', '') }}</textarea>
+            @error('description')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('thumb') class="is-invalid"  @enderror>
             <label for="thumb">Thumb</label>
-            <input type="url"max="250" required name="thumb">
+            <input type="url"maxlength="250" required name="thumb" value="{{ old('thumb', '') }}">
+            @error('thumb')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('price') class="is-invalid"  @enderror>
             <label for="price">Price</label>
-            <input type="number" required step=".5" name="price">
+            <input type="number" min="1" max="500" required step=".5" name="price"
+                value="{{ old('price', '') }}">
+            @error('price')
+                <div>
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('series') class="is-invalid"  @enderror>
             <label for="series">Serie</label>
-            <input type="text"max="50" required name="series">
+            <input type="text" maxlength="50" required name="series" value="{{ old('series', '') }}">
+            @error('series')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('sale_date') class="is-invalid"  @enderror>
             <label for="sale_date">Date</label>
-            <input type="date" required name="sale_date">
+            <input type="date" required name="sale_date" value="{{ old('sale_date', '') }}">
+            @error('sale_date')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('type') class="is-invalid"  @enderror>
             <label for="type">Type</label>
-            <input type="text"max="50" name="type">
+            <input type="text" maxlength="50" required name="type" value="{{ old('type', '') }}">
+            @error('type')
+                <div>
+                    {{ __('Sistema il type') }}
+                </div>
+            @enderror
         </div>
         <div>
             <input type="submit" value="Crea!">
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,57 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit comic</title>
 </head>
-<body>
-    
 
-<form method="POST" action="{{ route('comics.update', $comic->id) }}">
+<body>
+
+    <form method="POST" action="{{ route('comics.update', $comic->id) }}">
         @csrf
-        
         @method('PUT')
 
-
-
-
-
-        <div>
+        <div @error('title') class="is-invalid"  @enderror>
             <label for="title">Title</label>
-            <input type="text" name="title" value="{{ $comic->title }}">
+            <input type="text" maxlength="255" name="title"
+                value="{{ old('title', $comic->title) }}">
+            @error('title')
+                <div>
+                    {{ __('Sistema il titolo') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('description') class="is-invalid"  @enderror>
             <label for="description">Description</label>
-            <textarea name="description" cols="30" rows="10" value="{{ $comic->description }}"></textarea>
+            <textarea name="description" required cols="30" rows="10">{{ old('description', $comic->description) }}</textarea>
+            @error('description')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('thumb') class="is-invalid"  @enderror>
             <label for="thumb">Thumb</label>
-            <input type="url" name="thumb" value="{{ $comic->thumb }}">
+            <input type="url"maxlength="250" required name="thumb" value="{{ old('thumb', $comic->thumb) }}">
+            @error('thumb')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('price') class="is-invalid"  @enderror>
             <label for="price">Price</label>
-            <input type="number" step=".5" name="price" value="{{ $comic->price }}">
+            <input type="number" max="500" required step=".5" name="price"
+                value="{{ old('price', $comic->price) }}">
+            @error('price')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('series') class="is-invalid"  @enderror>
             <label for="series">Serie</label>
-            <input type="text" name="series" value="{{ $comic->series }}">
+            <input type="text" maxlength="50" required name="series" value="{{ old('series', $comic->series) }}">
+            @error('series')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('sale_date') class="is-invalid"  @enderror>
             <label for="sale_date">Date</label>
-            <input type="date" name="sale_date" value="{{ $comic->sale_date }}">
+            <input type="date" required name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+            @error('sale_date')
+                <div>
+                    {{ __('Sistema la descrizione') }}
+                </div>
+            @enderror
         </div>
-        <div>
+        <div @error('type') class="is-invalid"  @enderror>
             <label for="type">Type</label>
-            <input type="text" name="type" value="{{ $comic->type }}">
+            <input type="text" maxlength="50" required name="type" value="{{ old('type', $comic->type) }}">
+            @error('type')
+                <div>
+                    {{ __('Sistema il type') }}
+                </div>
+            @enderror
         </div>
         <div>
-            <input type="submit" value="Refresh!">
+            <input type="submit" value="Aggiorna!">
         </div>
     </form>
 
+</body>
 
 
 </body>
+
 </html>
